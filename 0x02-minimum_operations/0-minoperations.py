009b27a9f231522c8a2af61dcab40614cce5f779
PimehT <tareerepimeh@gmail.com>
2024-07-08 14:31:42 +0100

    min operations found

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,7 +12,7 @@ def prime_factors(n):
         factors.append(2)
         n //= 2
 
-    for i in range(3, int(n**0.5) + 1):
+    for i in range(3, int(n**0.5) + 1, 2):
         while n % i == 0:
             factors.append(i)
             n //= i
