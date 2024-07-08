78a62dd429a2271c8632eae95aef2deae7f4b27a
PimehT <tareerepimeh@gmail.com>
2024-07-08 14:10:54 +0100

    min operations found

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,26 +5,28 @@ when the text editor can only execute two commands: `Copy All` and
 `Paste`
 """
 
+
 def prime_factors(n):
     factors = []
     while n % 2 == 0:
         factors.append(2)
         n //= 2
-    
+
     for i in range(3, int(n**0.5) + 1):
         while n % i == 0:
             factors.append(i)
             n //= i
-    
+
     if n > 2:
         factors.append(n)
-    
+
     return factors
+
 
 def minOperations(n):
     if n < 2:
         return 0
-    if len(prime_factors(n)) == 1:
+    elif len(prime_factors(n)) == 1:
         return n
     else:
         return sum(prime_factors(n))
