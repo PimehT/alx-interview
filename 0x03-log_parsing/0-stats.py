a6149526d2dbc5f95b56afe0dbdd148825b0919d
PimehT <tareerepimeh@gmail.com>
2024-07-16 21:44:49 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,7 +12,7 @@ import sys
 # Initialize global variables
 total_size = 0
 status_codes = {
-    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    '200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 line_count = 0
 
 
@@ -40,7 +40,7 @@ try:
         # Extract file size
         try:
             total_size += int(parts[-1])
-            status_code = int(parts[-2])
+            status_code = parts[-2]
             if status_code in status_codes:
                 status_codes[status_code] += 1
         except (IndexError, ValueError):
