e8331ec643f703fff7d360547f12a18805a1efbe
PimehT <tareerepimeh@gmail.com>
2024-07-25 01:34:16 +0100

    0x04-utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+"""Determines if a given data set represents a valid UTF-8 encoding."""
 
 
 def validUTF8(data):
-    """method determines if given data set represents a valid UTF-8 encoding"""
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing bytes of data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
     num_bytes = 0
-    for num in data:
-        binary_rep = format(num, '#010b')[-8:]
+
+    for byte in data:
         if num_bytes == 0:
-            for bit in binary_rep:
-                if bit == '0':
-                    break
-                num_bytes += 1
-            if num_bytes == 0:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
                 return False
         else:
-            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
+            if (byte >> 6) != 0b10:
                 return False
-        num_bytes -= 1
+            num_bytes -= 1
+
     return num_bytes == 0
