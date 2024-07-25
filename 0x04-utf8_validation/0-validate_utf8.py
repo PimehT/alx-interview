137f61bf8f9793d163dc3377141384c906c8fef3
PimehT <tareerepimeh@gmail.com>
2024-07-25 20:22:28 +0100

    0x04-utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,22 +11,22 @@ def validUTF8(data):
 
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
-    """
-    num_bytes = 0
-
-    for byte in data:
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
+"""
+    following_bytes = 0
+    for num in data:
+        if following_bytes > 0:    
+            if not (num >> 6) == 0b10:
                 return False
-        else:
-            if (byte >> 6) != 0b10:
+            following_bytes -= 1
+        else:    
+            if (num >> 7) == 0:        
+                following_bytes = 0
+            elif (num >> 5) == 0b110:        
+                following_bytes = 1
+            elif (num >> 4) == 0b1110:        
+                following_bytes = 2
+            elif (num >> 3) == 0b11110:        
+                following_bytes = 3
+            else:        
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+    return following_bytes == 0
